c2963012ebdbe8b300720055bc3f1373db090d7c
anil.gautamm@gmail.com <anil1590@mocha4005.mochahost.com>
2018-03-09 10:36:33 -0500

    improvments in plugin

--- beyonic-payment-gateway.php
+++ beyonic-payment-gateway.php
@@ -54,13 +54,19 @@ function beyonic_woo_gw_init() {
             $this->init_form_fields();
             $this->init_settings();
             // Define user set variables
-            $this->title = "Beyonic Payments";
+            $this->title = "Pay with Mobile Money"; 
             $this->api_key = $this->get_option('api_key');
             $this->description = $this->get_option('description');
             $this->beyonic_api_version = 'v1';
             $this->ipn_url = site_url() . "?beyonic_ipn=1";
             add_action('woocommerce_update_options_payment_gateways_' . $this->id, array($this, 'process_admin_options'));
+
             add_action('admin_notices', array($this, 'beyonic_admin_notices'));
+
+            
+
+
+
         }
 
         /**
@@ -80,10 +86,19 @@ function beyonic_woo_gw_init() {
                 'description' => array(
                     'title' => __('Description', 'woocommerce'),
                     'type' => 'text',
-                    'description' => __('', 'woocommerce'),
-                    'default' => __('Enable your customers to pay for items with mobile money, using Beyonic', 'woocommerce')
+                    'description' => __('This description will be shown on your checkout page.', 'woocommerce'),
+                    'default' => __('Use mobile money from MPEAS, MNT or AIRTEL to pay for your order! <a href = "https://beyonic.com" > Powered by Beyonic payments </a>.', 'using Beyonic', 'woocommerce'),
+                    'desc_tip' => true,  
                 ),
-                'api_key' => array(
+                'notification_url' => array( 
+                    'title' => __('Callback notification URL', 'woocommerce'),
+                    'type' => 'text',
+                    'description' => __("This is the notification URL that will be used to send payment notifications to your website. You do not need to change it.NOTE:It must start with 'https'. If it does'n start with https, then that means that your website doesen't have a secure HTTPS certificate. ).", 'woocommerce'),
+                    'default' => get_site_url() . '?beyonic_ipn=1',
+                    'desc_tip' => true,
+                    'custom_attributes' => array('readonly' => 'readonly')
+                ),
+                'api_key' => array( 
                     'title' => __('Api Key', 'woocommerce'),
                     'type' => 'text',
                     'description' => __('Please enter your api key (you can get it from your Beyonic Profile).', 'woocommerce'),
@@ -105,7 +120,7 @@ function beyonic_woo_gw_init() {
             if (in_array($store_currency, $this->allowed_currency)) {
                 ?>
                 <h3><?php _e('Beyonic', 'woocommerce'); ?></h3>
-                <p><?php _e('Please fill in the section below to start accepting payments on your site. You can find all the required information in your Beyonic Profile'); ?> </p>
+                <p><?php _e('Please fill in the section below to start accepting payments on your site. You must first sign up for a Beyonic account at <a herf = "https://beyonic.com/signup" > https://beyonic.com/signup </a>. After that, you will find all the required information in your Beyonic Profile.'); ?> </p>
                 <table class="form-table">
                     <?php
                     // Generate the HTML For the settings form.
@@ -145,11 +160,11 @@ function beyonic_woo_gw_init() {
             $webhook = $wpdb->get_var($wpdb->prepare("SELECT option_value FROM wp_options WHERE option_name = %s", $meta_key));
 
             if (empty($webhook)) {
-                $url = str_replace("http", "https", $this->ipn_url);
+                $url = str_replace("http", "https", $this->ipn_url); 
                 try {
                     $hooks = Beyonic_Webhook::create(array(
-                                "event" => "collection.received",
-                                "target" => $url
+                        "event" => "collection.received",
+                        "target" => $url
                     ));
 
                     $wpdb->insert('wp_options', array('option_name' => 'Beyonic_Webhook', 'option_value' => 'Collection_received'));
@@ -169,14 +184,14 @@ function beyonic_woo_gw_init() {
 
             try {
                 $request = Beyonic_Collection_Request::create(array(
-                            "phonenumber" => $order->billing_phone,
-                            "first_name" => $order->billing_first_name,
-                            "last_name" => $order->billing_last_name,
-                            "amount" => $order->get_total(),
-                            "success_message" => 'Thank you for your payment!',
-                            "send_instructions" => true,
-                            "currency" => $order->get_order_currency(),
-                            "metadata" => array("order_id" => $order_id)
+                    "phonenumber" => $order->billing_phone,
+                    "first_name" => $order->billing_first_name,
+                    "last_name" => $order->billing_last_name,
+                    "amount" => $order->get_total(),
+                    "success_message" => 'Thank you for your payment!',
+                    "send_instructions" => true,
+                    "currency" => $order->get_order_currency(),
+                    "metadata" => array("order_id" => $order_id)
                 ));
 
                 $beyonic_collection_id = intval($request->id);
@@ -192,7 +207,7 @@ function beyonic_woo_gw_init() {
                 );
             } catch (Exception $exc) {
                 $notice = $exc->responseBody;
-               
+
                 // If function should we use?
                 if (function_exists("wc_add_notice")) {
                     // Use the new version of the add_error method
@@ -238,6 +253,13 @@ function beyonic_woo_gw_init() {
                 <p>Https must be enabled to use beyonic payments. If you are testing, please see the testing section of the Beyonic api documentation at https://apidocs.beyonic.com for how to use test https certificates for instant payment notifications.</p>
             </div>
             <?php
+
+            if(empty($this->api_key)) { ?>
+            <div id="message" class="notice notice-warning is-dismissible">
+                <p> Beyonic is almost ready. To get started, <a href = "https://app.beyonic.com/signup/"> set your Beyonic account key </a> </p>
+            </div>
+
+            <?php } 
         }
 
     }
