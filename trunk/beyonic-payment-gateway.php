46c472348947d74fc765403cbe8f7640e9c0211d
Kingsley Robertovich <kingsley.ndiewo@beyonic.com>
2020-09-10 09:31:43 +0300

    Updated version to match SVN
    Minor change

--- trunk/beyonic-payment-gateway.php
+++ trunk/beyonic-payment-gateway.php
@@ -5,7 +5,7 @@
  * Description: Receive payments using the Beyonic.
  * Author: beyonic
  * Author URI: http://beyonic.com/
- * Version: 2.0.0
+ * Version: 2.0.12
  */
 if (!defined('ABSPATH'))
     exit; // Exit if accessed directly
